322669ccfcdba3d682b0bcc011a6fc1097b3a9fc
siddiqahmed882 <siddiqahmed882@gmail.com>
2022-09-01 00:56:06 +0500

    updated root link

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [BlogController::class, 'index'])
+    ->name('blog.index');
+
 Route::get('/blog', [BlogController::class, 'index'])
     ->name('blog.index');
 
